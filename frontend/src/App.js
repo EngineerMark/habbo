aa5e9e10a4af2a242878f5fde0967fe2b358e6da
Mark Meijer <engineermarkpro@gmail.com>
2023-05-26 12:32:59 +0200

    Added basics for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Box, CssBaseline, ThemeProvider, Typography, createTheme } from '@mui/material';
+import Header from './Components/Header';
+
+const theme = {
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#124975'
+    }
+  },
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme(theme)}>
+      <CssBaseline />
+      <Box>
+        <Header />
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+function Header(props){
+    return(
+        <Box sx={{
+            backgroundColor: '#7ecaee',
+            height: '150px'
+        }}>
+
+        </Box>
+    )
+}
+
+export default Header;
